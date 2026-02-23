694f77af7c0cdf8f84fb62a1452318ff5f7f97d0
satwikdj77 <satwikdj77@gmail.com>
2026-02-23 19:52:33 +0530

    Add files via upload

---
+++ OOPSBannerApp.java
@@ -0,0 +1,15 @@
+/** 
+*  @author  Satwik
+*  @version  3.0 
+*/ 
+public  class  OOPSBannerApp  { 
+public  static  void  main (String[] args)  { 
+System.out.println(String.join( "" ,  "  OOO  " ,  "  OOO  " ,  " PPPP  " ,  " SSSS  " )); 
+System.out.println(String.join( "" ,  " O   O  " ,  " O   O " ,  " P   P " ,  " S     " )); 
+System.out.println(String.join( "" ,  " O   O  " ,  " O   O " ,  " P   P " ,  " S     " )); 
+System.out.println(String.join( "" ,  " O   O  " ,  " O   O " ,  " PPPP  " ,  " SSS   " )); 
+System.out.println(String.join( "" ,  " O   O  " ,  " O   O " ,  " P     " ,  "    S  " )); 
+System.out.println(String.join( "" ,  " O   O  " ,  " O   O " ,  " P     " ,  "    S  " )); 
+System.out.println(String.join( "" ,  "  OOO  " ,  "  OOO  " ,  " P     " ,  " SSS   " )); 
+} 
+} 
